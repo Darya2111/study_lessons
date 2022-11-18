996ac48e1ebb33eb4cc2ccd32f53c2f625250e7a
Даша <Teddi21girl@gmail.com>
2022-11-18 11:52:01 +0300

    added webTest, simpleTest

---
+++ src/test/java/tests/MenuItem.java
@@ -0,0 +1,11 @@
+package tests;
+
+public enum MenuItem {
+    IMG("Картинки"), VID("Видео");
+
+    public final String rusName;
+
+    MenuItem(String rusName) {
+        this.rusName = rusName;
+    }
+}
